7f5872bfa26e31a03f8447ece0690052cc2bb9da
eran <shush6@gmail.com>
2019-03-03 11:52:17 +0200

    added basic reducer

--- src/ReviewList/ReviewsList.js
+++ src/ReviewList/ReviewsList.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import './ReviewsList.css';
 import ReviewListItem from "../ReviewListItem/ReviewListItem";
 
-const ReviewsList = ({reviews = []}) => {
+function reducer(state, action) {
+    switch (action.type) {
+
+        default:
+            throw new Error('Reducer error: action does not exist');
+    }
+}
+
+const ReviewsList = () => {
+    const [state, dispatch] = useReducer(reducer, {reviews: [{name: 'eran', comment: 'this is great', avatar: 'sdsdsd'}]});
     return (
         <React.Fragment>
             <div className="review-list-container">
-                {reviews.map((review) => <ReviewListItem />)}
+                {state.reviews.map((review) => <ReviewListItem review={review}/>)}
             </div>
         </React.Fragment>);
 };
